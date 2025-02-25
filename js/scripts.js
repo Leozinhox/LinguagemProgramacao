63940c38e389f21b416ec3942077ded12765f698
Leozinhox <leonardo.santos307@fatec.sp.gov.br>
2025-02-25 09:10:53 -0300

    exercicio 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 function seguir(){
     alert('Você está seguindo Jesus.')
 }
+// Exercicio 1
+function exe1(){
+    // vamos usar DOM para recuperar os dados do HTML
+    // DOM - Document Object Model
+    //converter texto para numero NUMBER()
+
+    let nro1 = number(document.getElementById("nro1").value)
+    let nro2 = number(document.getElementById("nro2").value)
+    // vamos fazer a subtração
+    let sub = nro1 - nro2
+    // vamos  mostrar o resultado ao usuario
+    //alert(" A Subtração é " + sub)
+    document.getElementById("sub").innerText = "O resultado é " + sub
+}
